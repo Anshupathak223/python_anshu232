99afcf983d6f3a8befc4abc3243701bcd63dc1b6
Anshupathak223 <12anshupathak@gmail.com>
2025-08-28 14:48:19 +0530

    python

---
+++ 1.py
@@ -0,0 +1,25 @@
+a=5
+print(a)
+var1=a
+print(id(var1))
+print(type(var1))
+var2=True
+print(id(var2))
+print(type(var2))
+var3='8'
+print(id(var3))
+print(type(var3))
+var4=10.9
+print(id(var4))
+print(type(var4))
+var5=10+3j
+print(id(var5))
+print(type(var5))
+var6="hello world"
+print(len(var6))
+txt="where are you going"
+print("where" in txt)
+print("anshu" in txt)
+print("free" not in txt)
+print(txt.upper())
+print(txt.lower())
