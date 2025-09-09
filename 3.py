fe39c97ecb6d34cc26332a09351d0ccd9babefac
Anshupathak223 <12anshupathak@gmail.com>
2025-09-09 12:24:36 +0530

    day3

---
+++ 3.py
@@ -0,0 +1,28 @@
+# day= int(input())
+# match day:
+    # case 1:
+        # print("hello")
+    # case 2:
+        # print("world")
+    # case 3:
+        # print("hii")
+    # case 4:
+        # print("Anshu")
+    # case 5:
+        # print("Pathak")
+    # case _:
+        # print("Error")
+        # case 1|2|3|4|5 if day==5:
+            # print("yesss")
+        # case _:
+        #    print("error")
+        #    while loop
+# while day <10:
+    # print(day)
+    # day+=1
+a= 7
+b=int(input())
+if a==b:
+    print("Yeah,You guessed it correct")
+else:
+    print("Sorry! better luck next time")
